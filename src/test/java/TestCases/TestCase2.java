05ccba9297067221cf8f7c28d18dccee84e377b5
Vamsi <vamsivamc3626@gmail.com>
2023-08-11 09:34:33 +0530

    V's modified commit on 2nd test case

--- src/test/java/TestCases/TestCase2.java
+++ src/test/java/TestCases/TestCase2.java
@@ -5,9 +5,6 @@ public class TestCase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a test Written By VPenugurthi");
-		System.out.println("This is a test Written By VPenugurthi");
-		System.out.println("This is a test Written By VPenugurthi");
-		System.out.println("This is a test Written By VPenugurthi");
 
 	}
 
